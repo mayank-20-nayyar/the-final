38eb90ff1b4ae14a5623d3d7d42fc25fdf2919b0
mowgli-m <mayanknayyar95@gmail.com>
2017-01-17 18:05:05 +0530

    the updated main and map activity

--- app/src/main/java/com/example/mayank/singleboom/MapsActivity.java
+++ app/src/main/java/com/example/mayank/singleboom/MapsActivity.java
@@ -10,9 +10,13 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -31,6 +35,9 @@ import com.nightonke.boommenu.BoomButtons.TextOutsideCircleButton;
 import com.nightonke.boommenu.BoomMenuButton;
 import com.nightonke.boommenu.Util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsActivity extends AppCompatActivity implements
         OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
@@ -39,19 +46,21 @@ public class MapsActivity extends AppCompatActivity implements
         GoogleMap.OnMapLongClickListener
          {
 
-    //Our Map
+
     private GoogleMap mMap;
 
-    //To store longitude and latitude from map
     public double longitude;
     public double latitude;
 
-    //Buttons
-   // private ImageButton buttonSave;
-   // private ImageButton buttonCurrent;
-   // private ImageButton buttonView;
+             public int i;
+             Button b;
+             public Bitmap orginalBitmap;
+             public List<Marker> markers = new ArrayList<Marker>();
+             public List<Marker> markerNames = new ArrayList<Marker>();
+             public List<String> friendName = new ArrayList<String>();
 
-    //Google ApiClient
+             public int kk = 0;
+
     private GoogleApiClient googleApiClient;
 
              private BoomMenuButton bmb;
@@ -200,6 +209,50 @@ public class MapsActivity extends AppCompatActivity implements
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 
+             public void updateMoveMap(){
+
+                 String msg = latitude + ", "+longitude;
+
+
+                 LatLng latLng = new LatLng(latitude, longitude);
+
+
+                 mMap.addMarker(new MarkerOptions()
+                         .position(latLng) //setting position
+                         .draggable(true) //Making the marker draggable
+                         .title("Current Location")); //Adding a title
+
+
+                 mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+
+                // mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+
+                 Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+             }
+
+             public void getUpdatedOwnLocation(){
+                 mMap.clear();
+
+                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                     // TODO: Consider calling
+
+                     return;
+                 }
+                 Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+                 if (location != null) {
+
+                     longitude = location.getLongitude();
+                     latitude = location.getLatitude();
+                     mMap.animateCamera(CameraUpdateFactory.zoomTo(40));
+
+
+                     updateMoveMap();
+
+                 }
+             }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -259,9 +312,6 @@ public class MapsActivity extends AppCompatActivity implements
     }
 
 
-            //getCurrentLocation();
-            //new updatemymapp().execute();
-
 
 
     void startThread()
@@ -270,7 +320,7 @@ public class MapsActivity extends AppCompatActivity implements
 
             @Override
             public void run(){
-                //
+
                 try
                 {
                     for (;;)
@@ -279,12 +329,7 @@ public class MapsActivity extends AppCompatActivity implements
                             @Override
                             public void run() {
                                 try {
-
-
-                                getCurrentLocation();
-
-
-
+                                    getUpdatedOwnLocation();
                                 }
                                 catch(Exception e)
                                 {
@@ -303,80 +348,7 @@ public class MapsActivity extends AppCompatActivity implements
         };
         th.start();
     }
-             void startFriendThread()
-             {
-                 Log.e("inside ","SFT");
-                 Thread th=new Thread(){
-
-                     @Override
-                     public void run(){
-                         //
-
-                         try
-                         {
-                             Log.e("inside","try");
-                             for (i =0;i<3;i++)
-                             {
-                                 Log.e("val of i ",i+"");
-                                 runOnUiThread(new Runnable() {
-                                     @Override
-                                     public void run() {
-                                         try {
-                                             double latitude = 28.6139;
-                                             double longitude = 77.2090;
-
-
-                                             if(i!=0) {
-                                                 for (int k = 0; k < markers.size(); k++) {
-                                                     Marker m = markers.get(k);
-                                                     m.remove();
-                                                 }
-                                             }
-                                             for (int j = 0; j < 3; j++) {
-                                                 if(i==0)
-                                                 {
-                                                     latitude = latitude/2;
-
-                                                 }
-                                                 if(i==1)
-                                                 {
-                                                     longitude = longitude/2;
-                                                 }
-                                                 if(i==2)
-                                                 {
-                                                     latitude = latitude/2;
-                                                     longitude = longitude/2;
-                                                 }
-                                                 latitude = latitude + (3*j);
-                                                 longitude += (4*j);
-                                                 getUpdatedCurrentLocation(latitude, longitude);
-                                             }
-                                             for(int x=0;x<markers.size();x++)
-                                             {
-                                                 Marker mm = markers.get(x);
-                                                 mm.showInfoWindow();
-                                             }
-
-                                         }
-                                         catch(Exception e)
-                                         {
-                                             e.printStackTrace();
-                                         }
-                                     }
-                                 });
-                                 Thread.sleep(5000);
-
-                             }
-                         }
-                         catch (InterruptedException e) {
-                         }
-
-                     }
-                 };
-                 th.start();
-             }
-
-             public void getUpdatedCurrentLocation(double latitude, double longitude) {
+             public void getUpdatedCurrentLocation(double latitude, double longitude, String name) {
                  if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                      // TODO: Consider calling
 
@@ -385,7 +357,7 @@ public class MapsActivity extends AppCompatActivity implements
                  }
                  Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
 
-                 //Getting longitude and latitude
+
 
 
                  LatLng latLngg = new LatLng(latitude, longitude);
@@ -423,52 +395,160 @@ public class MapsActivity extends AppCompatActivity implements
                  // Marker marker = mMap.addMarker(new MarkerOptions().position(latLngg).icon(BitmapDescriptorFactory.fromResource(R.drawable.mark)));
                  // marker.showInfoWindow();
 
+/*
+        Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(),
+                R.drawable.marker3);
 
-                 Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(),
-                         R.drawable.marker3);
-
-                 Bitmap bmp2 = bitmap1.copy(bitmap1.getConfig(), true);
+        Bitmap bmp2 = bitmap1.copy(bitmap1.getConfig(), true);*/
 
        /* Bitmap.Config conf = Bitmap.Config.ARGB_8888;
         Bitmap bitmap1 = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(),
                 R.drawable.markk));*/
-                 Bitmap bitmap2  = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(),
-                         R.drawable.hot));
+       /* Bitmap bitmap2  = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(),
+                R.drawable.hot));
 
-                 Bitmap hott = bitmap2.copy(bitmap2.getConfig(), true);
-                 int bitmap1Width = bitmap1.getWidth();
-                 int bitmap1Height = bitmap1.getHeight();
-                 int bitmap2Width = bitmap2.getWidth();
-                 int bitmap2Height = bitmap2.getHeight();
+        Bitmap hott = bitmap2.copy(bitmap2.getConfig(), true);
+       int bitmap1Width = bitmap1.getWidth();
+        int bitmap1Height = bitmap1.getHeight();
+        int bitmap2Width = bitmap2.getWidth();
+        int bitmap2Height = bitmap2.getHeight();
 
-                 float marginLeft = (float) (bitmap1Width * 0.5 - bitmap2Width * 0.5);
-                 float marginTop = (float) (bitmap1Height * 0.5 - bitmap2Height * 0.5);
-
+        float marginLeft = (float) (bitmap1Width * 0.5 - bitmap2Width * 0.5);
+        float marginTop = (float) (bitmap1Height * 0.5 - bitmap2Height * 0.5);
+*/
 
        /*Bitmap overlayBitmap = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(),
                R.drawable.markk));
-       */ Canvas canvas = new Canvas(bmp2);
+       */
 
-        /*canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),
+        /*Canvas canvas = new Canvas(bmp2);
+
+        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),
                 R.drawable.markk), new Matrix(), null);
-        */canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),
-                         R.drawable.hot), marginLeft, marginTop, null);
+        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),
+               R.drawable.hot), marginLeft, marginTop, null);
+*/
 
-                 Marker marker = mMap.addMarker(new MarkerOptions().position(latLngg)
-                         .icon(BitmapDescriptorFactory.fromBitmap(bmp2))
-                         // Specifies the anchor to be at a particular point in the marker image.
+                 String text = name;
+                 Bitmap b = Bitmap.createBitmap(650, 240, Bitmap.Config.ARGB_8888);
+                 Canvas c = new Canvas(b);
+                 c.drawBitmap(b, 0, 0, null);
+                 TextPaint textPaint = new TextPaint();
+                 textPaint.setAntiAlias(true);
+                 textPaint.setTextSize(getResources().getDimension(R.dimen.textsize));
+                 StaticLayout sl= new StaticLayout(text, textPaint, b.getWidth()-8, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+                 c.translate(6, 40);
+                 sl.draw(c);
+
+                 Marker markerName = mMap.addMarker(new MarkerOptions().position(latLngg)
+                         .icon(BitmapDescriptorFactory.fromBitmap(b))
                          .anchor(0.5f, 1));
 
+                 Marker marker = mMap.addMarker(new MarkerOptions()
+                         .position(latLngg)
+                         .draggable(true)
+                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+
+                 //marker.showInfoWindow();
+
                  mMap.moveCamera(CameraUpdateFactory.newLatLng(latLngg));
+                 mMap.getUiSettings().setScrollGesturesEnabled(true);
+
 
                  //Animating the camera
-                 mMap.animateCamera(CameraUpdateFactory.zoomTo(2));
+                 mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
 
                  markers.add(marker);
-
+                 markerNames.add(markerName);
 
 
              }
+
+             void startFriendThread()
+             {
+                 Log.e("inside ","SFT");
+                 Thread th=new Thread(){
+
+                     @Override
+                     public void run(){
+                         //
+
+                         try
+                         {
+                             Log.e("inside","try");
+                             friendName.add("Mayank");
+                             friendName.add("Vaibhav");
+                             friendName.add("Dopu");
+                             friendName.add("harshit");
+                             friendName.add("may");
+                             friendName.add("Dope");
+                             friendName.add("M");
+                             friendName.add("V");
+                             friendName.add("D");
+
+
+                             for (i =0;i<3;i++)
+                             {
+                                 Log.e("val of i ",i+"");
+                                 runOnUiThread(new Runnable() {
+                                     @Override
+                                     public void run() {
+                                         try {
+                                             double latitude = 28.6139;
+                                             double longitude = 77.2090;
+                                             String name = "";
+
+                                             if(i!=0) {
+                                                 for (int k = 0; k < markers.size(); k++) {
+                                                     Marker m = markers.get(k);
+                                                     m.remove();
+                                                     Marker mm = markerNames.get(k);
+                                                     mm.remove();
+                                                 }
+                                             }
+                                             for (int j = 0; j < 3; j++) {
+                                                 if(i==0)
+                                                 {
+                                                     latitude = latitude/2;
+
+
+                                                 }
+                                                 if(i==1)
+                                                 {
+                                                     longitude = longitude/2;
+                                                 }
+                                                 if(i==2)
+                                                 {
+                                                     latitude = latitude/2;
+                                                     longitude = longitude/2;
+                                                 }
+                                                 latitude = latitude + (3*j);
+                                                 longitude += (4*j);
+                                                 name = friendName.get(j + kk);
+                                                 Log.e("the value of i+j",(j + kk) + "");
+                                                 Log.e("the value of friend",name);
+                                                 getUpdatedCurrentLocation(latitude, longitude, name);
+                                             }
+                                             kk += 3;
+                                         }
+                                         catch(Exception e)
+                                         {
+                                             e.printStackTrace();
+                                         }
+                                     }
+                                 });
+                                 Thread.sleep(5000);
+
+                             }
+                         }
+                         catch (InterruptedException e) {
+                         }
+
+                     }
+                 };
+                 th.start();
+             }
+
 
 
 
@@ -477,31 +557,5 @@ public class MapsActivity extends AppCompatActivity implements
 
          }
 
-/*
-class updatemymapp extends AsyncTask<Void,Void,Void>
-{
-    MapsActivity ma = new MapsActivity();
 
-
-
-    @Override
-    protected Void doInBackground(Void... params) {
-        int t=200;
-        while(t!=0) {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            ma.getCurrentLocation();
-        */
-/*    ma.latitude = marker.getPosition().latitude;
-            ma.longitude = marker.getPosition().longitude;
-            ma.moveMap();*//*
-
-            t--;
-        }
-        return null;
-    }
-}*/
 
