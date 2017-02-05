1a1c4eb506fa27b696f4788d8196342bc7fe56b3
mowgli-m <mayanknayyar95@gmail.com>
2017-02-06 01:01:01 +0530

    updated login and signup

--- app/src/main/java/com/example/mayank/singleboom/LoginActivity.java
+++ app/src/main/java/com/example/mayank/singleboom/LoginActivity.java
@@ -6,6 +6,7 @@ package com.example.mayank.singleboom;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,6 +17,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
@@ -24,11 +44,21 @@ public class LoginActivity extends AppCompatActivity {
     private static final int REQUEST_SIGNUP = 0;
     AnimationDrawable rocketAnimation;
 
-    @InjectView(R.id.input_email) EditText _emailText;
-    @InjectView(R.id.input_password) EditText _passwordText;
-    @InjectView(R.id.btn_login) Button _loginButton;
-    @InjectView(R.id.link_signup) TextView _signupLink;
-    @InjectView(R.id.input_no) EditText _mobile;
+    @InjectView(R.id.input_email) EditText emailText;
+    @InjectView(R.id.input_password) EditText passwordText;
+    @InjectView(R.id.btn_login) Button loginButton;
+    @InjectView(R.id.link_signup) TextView signupLink;
+    @InjectView(R.id.input_no) EditText mobile;
+
+    String email;
+    String number;
+    String password;
+
+    HttpClient client;
+    HttpPost post;
+    HttpResponse response;
+    // Context context;
+    public String res;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,7 +69,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
 
-        _loginButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -47,7 +77,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        _signupLink.setOnClickListener(new View.OnClickListener() {
+        signupLink.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -67,7 +97,7 @@ public class LoginActivity extends AppCompatActivity {
             return;
         }
 
-        _loginButton.setEnabled(false);
+        loginButton.setEnabled(false);
 
         final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
                 R.style.AppTheme);
@@ -75,13 +105,13 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog.setMessage("Authenticating...");
         progressDialog.show();
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-        String number = _mobile.getText().toString();
+        email = emailText.getText().toString();
+        password = passwordText.getText().toString();
+        number = mobile.getText().toString();
 
         // TODO: Implement your own authentication logic here.
 
-        new android.os.Handler().postDelayed(
+        /*new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
                         // On complete call either onLoginSuccess or onLoginFailed
@@ -90,8 +120,93 @@ public class LoginActivity extends AppCompatActivity {
                         progressDialog.dismiss();
                     }
                 }, 3000);
+        */
+        startAsync();
+
+
+
+
     }
 
+    void startAsync(){
+        new LoadTaskLogin(this).execute("https://sporophoric-reservo.000webhostapp.com/signUp.php");
+    }
+    boolean makeConnection(String url) {
+        try {
+            client = new DefaultHttpClient();
+            final HttpParams httpParams = client.getParams();
+            HttpConnectionParams.setConnectionTimeout(httpParams, 10000); // 10 seconds
+            HttpConnectionParams.setSoTimeout(httpParams, 10000);
+
+            //post = new HttpPost("http://192.168.43.3/signUp.php");
+            post = new HttpPost("https://sporophoric-reservo.000webhostapp.com/signUp.php");
+
+            return true;
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public boolean generatePostRequest() {
+        try {
+            //String nameStr = name.toString();
+            String nameStr = "this is  mayank the sex and handjob";
+            String eMailStr = "sex@600";//eMail.toString();
+            String passWordStr = "sexyy";//passWord.toString();
+
+            Log.e("this is email",eMailStr);
+            Log.e("this is pass",passWordStr);
+            Log.e("this is name",nameStr);
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+            nameValuePairs.add(new BasicNameValuePair("name", nameStr));
+            nameValuePairs.add(new BasicNameValuePair("email", eMailStr));
+            nameValuePairs.add(new BasicNameValuePair("password", passWordStr));
+
+
+           /* List<NameValuePair> urlParameters = new ArrayList<NameValuePair>(1);
+            urlParameters.add(new BasicNameValuePair("name", name));
+           */// urlParameters.add(new BasicNameValuePair("phone", aphone));
+
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            response = client.execute(post);
+        } catch (UnsupportedEncodingException e) {
+
+            e.printStackTrace();
+            return false;
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public String getRequestResponse() {
+        //String res;
+
+        try {
+            res = EntityUtils.toString(response.getEntity());
+            return res;
+        } catch (org.apache.http.ParseException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void nextStep(){
+        Log.e("this is in nextStep","this");
+        //Log.e("app context", getApplicationContext()+"");
+        Intent i = new Intent(LoginActivity.this, MapsActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -116,7 +231,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLoginSuccess() {
-        _loginButton.setEnabled(true);
+        loginButton.setEnabled(true);
         Intent i=new Intent(this,MapsActivity.class);
         startActivity(i);
         finish();
@@ -125,29 +240,124 @@ public class LoginActivity extends AppCompatActivity {
     public void onLoginFailed() {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
 
-        _loginButton.setEnabled(true);
+        loginButton.setEnabled(true);
     }
 
     public boolean validate() {
         boolean valid = true;
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+        email = emailText.getText().toString();
+        password = passwordText.getText().toString();
 
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
+            emailText.setError("enter a valid email address");
             valid = false;
         } else {
-            _emailText.setError(null);
+            emailText.setError(null);
         }
 
         if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            passwordText.setError("between 4 and 10 alphanumeric characters");
             valid = false;
         } else {
-            _passwordText.setError(null);
+            passwordText.setError(null);
         }
 
         return valid;
     }
 }
+
+
+class LoadTaskLogin extends AsyncTask<String, String, String> {
+
+    LoginActivity ma ;
+    boolean check_flag = true;
+
+    public LoadTaskLogin(LoginActivity ma) {
+        this.ma = ma;
+    }
+
+
+
+
+   /* Context context;
+    public LoadTask(Context context) {
+        this.context = context.getApplicationContext();
+    }*/
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Log.e("inside","pre");
+    }
+
+    protected String doInBackground(String... params) {
+        if (check_flag) {
+
+            check_flag = ma.makeConnection(params[0]);
+            Log.e("f", check_flag + "");
+        }
+
+        if (check_flag) {
+
+            check_flag = ma.generatePostRequest();
+            Log.e("s", check_flag + "");
+        }
+
+        if (check_flag) {
+
+            return ma.getRequestResponse();
+
+        }
+
+        return null; // null is returned if request failed
+
+
+    }
+
+    @Override
+    protected void onPostExecute(String res) {
+
+        if (ma.res != null) {
+            try {
+
+
+
+                Log.e("before", "object");
+                Log.e("the value of ma.res", ma.res);
+                JSONObject obj = new JSONObject(ma.res);
+                //Log.e("this is", obj+"");
+                //JSONArray j = new JSONArray();
+                // /JSONArray arr = obj.getJSONArray("ress");
+                String ss = obj.getString("mes");
+                // int length = arr.length();
+                Log.e("the value object", ss + "this should be sex bro");
+                if (ss.equals("yes")) {
+                    Log.e("this is fucking", "idiotic");
+
+                    //context.startActivity(new Intent(context, MapsActivity.class));
+                    ma.nextStep();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        // Toast.makeText(this,ma.id,Toast.LENGTH_LONG).show();
+        Log.e("task done","bitchess!!!1");
+
+        super.onPostExecute(ma.res);
+
+    }
+
+//    @Override
+//    public void onAttach(Activity activity){
+//        this.activity = activity;
+//
+
+}
+
+
+
+
--- app/src/main/java/com/example/mayank/singleboom/SignUpActivity.java
+++ app/src/main/java/com/example/mayank/singleboom/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.example.mayank.singleboom;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,6 +11,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
@@ -24,6 +45,16 @@ public class SignUpActivity extends AppCompatActivity {
     @InjectView(R.id.btn_signup) Button _signupButton;
     @InjectView(R.id.link_login) TextView _loginLink;
     @InjectView(R.id.input_no) EditText _mobile;
+
+    String email;
+    String number;
+    String password;
+    String name;
+    HttpClient client;
+    HttpPost post;
+    HttpResponse response;
+    // Context context;
+    public String res;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,14 +95,14 @@ public class SignUpActivity extends AppCompatActivity {
         progressDialog.setMessage("Creating Account...");
         progressDialog.show();
 
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-        String number = _mobile.getText().toString();
+        name = _nameText.getText().toString();
+        email = _emailText.getText().toString();
+        password = _passwordText.getText().toString();
+        number = _mobile.getText().toString();
 
         // TODO: Implement your own signup logic here.
 
-        new android.os.Handler().postDelayed(
+        /*new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
                         // On complete call either onSignupSuccess or onSignupFailed
@@ -80,10 +111,90 @@ public class SignUpActivity extends AppCompatActivity {
                         // onSignupFailed();
                         progressDialog.dismiss();
                     }
-                }, 3000);
+                }, 3000);*/
+
+        startAsync();
     }
 
+    void startAsync(){
+        new LoadTaskLoginSignUp(this).execute("https://sporophoric-reservo.000webhostapp.com/signUp.php");
+    }
+    boolean makeConnection(String url) {
+        try {
+            client = new DefaultHttpClient();
+            final HttpParams httpParams = client.getParams();
+            HttpConnectionParams.setConnectionTimeout(httpParams, 10000); // 10 seconds
+            HttpConnectionParams.setSoTimeout(httpParams, 10000);
 
+            //post = new HttpPost("http://192.168.43.3/signUp.php");
+            post = new HttpPost("https://sporophoric-reservo.000webhostapp.com/signUp.php");
+
+            return true;
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public boolean generatePostRequest() {
+        try {
+            //String nameStr = name.toString();
+            String nameStr = "this is  mayank the sex and handjob";
+            String eMailStr = "sex@600";//eMail.toString();
+            String passWordStr = "sexyy";//passWord.toString();
+
+            Log.e("this is email",eMailStr);
+            Log.e("this is pass",passWordStr);
+            Log.e("this is name",nameStr);
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+            nameValuePairs.add(new BasicNameValuePair("name", nameStr));
+            nameValuePairs.add(new BasicNameValuePair("email", eMailStr));
+            nameValuePairs.add(new BasicNameValuePair("password", passWordStr));
+
+
+           /* List<NameValuePair> urlParameters = new ArrayList<NameValuePair>(1);
+            urlParameters.add(new BasicNameValuePair("name", name));
+           */// urlParameters.add(new BasicNameValuePair("phone", aphone));
+
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            response = client.execute(post);
+        } catch (UnsupportedEncodingException e) {
+
+            e.printStackTrace();
+            return false;
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public String getRequestResponse() {
+        //String res;
+
+        try {
+            res = EntityUtils.toString(response.getEntity());
+            return res;
+        } catch (org.apache.http.ParseException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void nextStep(){
+        Log.e("this is in nextStep","this");
+        //Log.e("app context", getApplicationContext()+"");
+        Intent i = new Intent(SignUpActivity.this, MapsActivity.class);
+        startActivity(i);
+    }
 
     @Override
     public void onBackPressed() {
@@ -139,3 +250,97 @@ public class SignUpActivity extends AppCompatActivity {
         return valid;
     }
 }
+
+class LoadTaskLoginSignUp extends AsyncTask<String, String, String> {
+
+    SignUpActivity ma ;
+    boolean check_flag = true;
+
+    public LoadTaskLoginSignUp(SignUpActivity ma) {
+        this.ma = ma;
+    }
+
+
+
+
+   /* Context context;
+    public LoadTask(Context context) {
+        this.context = context.getApplicationContext();
+    }*/
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Log.e("inside","pre");
+    }
+
+    protected String doInBackground(String... params) {
+        if (check_flag) {
+
+            check_flag = ma.makeConnection(params[0]);
+            Log.e("f", check_flag + "");
+        }
+
+        if (check_flag) {
+
+            check_flag = ma.generatePostRequest();
+            Log.e("s", check_flag + "");
+        }
+
+        if (check_flag) {
+
+            return ma.getRequestResponse();
+
+        }
+
+        return null; // null is returned if request failed
+
+
+    }
+
+    @Override
+    protected void onPostExecute(String res) {
+
+        if (ma.res != null) {
+            try {
+
+
+
+                Log.e("before", "object");
+                Log.e("the value of ma.res", ma.res);
+                JSONObject obj = new JSONObject(ma.res);
+                //Log.e("this is", obj+"");
+                //JSONArray j = new JSONArray();
+                // /JSONArray arr = obj.getJSONArray("ress");
+                String ss = obj.getString("mes");
+                // int length = arr.length();
+                Log.e("the value object", ss + "this should be sex bro");
+                if (ss.equals("yes")) {
+                    Log.e("this is fucking", "idiotic");
+
+                    //context.startActivity(new Intent(context, MapsActivity.class));
+                    ma.nextStep();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        // Toast.makeText(this,ma.id,Toast.LENGTH_LONG).show();
+        Log.e("task done","bitchess!!!1");
+
+        super.onPostExecute(ma.res);
+
+    }
+
+//    @Override
+//    public void onAttach(Activity activity){
+//        this.activity = activity;
+//
+
+}
+
+
+
+
